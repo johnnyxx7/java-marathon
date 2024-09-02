788b4e8d0c08e83eba9498caf7c3a919e64e7a1e
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 12:51:09 -0300

    Create Aula03TiposPrimitivosExercicio.java

---
+++ src/academy/devdojo/javamarathon/introduction/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,19 @@
+package academy.devdojo.javamarathon.introduction;
+
+public class Aula03TiposPrimitivosExercicio {
+    /*
+     Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+     Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salario>, na data <data>.
+     */
+
+    public static void main (String[] args) {
+        String nome = "Johnny";
+        String endereco = "Av. São João";
+        double salario = 3512;
+        String dataRecebimentoSalario = "02/09/2024";
+        String report = "Eu, " + nome + " morando no endereço: " + endereco + ", confirmo que recebi o salário de R$" +
+                salario + ", na data " + dataRecebimentoSalario;
+
+        System.out.println(report);
+    }
+}
