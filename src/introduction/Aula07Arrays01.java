88f41a763ff8239de6cb4870b6ee2edd6d8ff93e
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 14:46:11 -0300

    att commit

---
+++ src/introduction/Aula07Arrays01.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        int idades[] = new int[3];
+
+        idades[0] = 15;
+        idades[1] = 18;
+        idades[2] = 23;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
