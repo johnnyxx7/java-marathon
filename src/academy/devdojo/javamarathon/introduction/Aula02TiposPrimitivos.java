22b136b984145d23fd2cb54a8a267952fecd7ec2
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 11:56:11 -0300

    atualização
    primeiro commit

--- src/academy/devdojo/javamarathon/introduction/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/javamarathon/introduction/Aula02TiposPrimitivos.java
@@ -5,6 +5,16 @@ public class Aula02TiposPrimitivos {
         // int, double, float, char, byte, short, long, boolean
 
         int idade = 10;
+        long numeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 127;
+        short idadeShort = 18;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u8841';
         System.out.println("A idade é " + idade + " anos.");
+        System.out.println(verdadeiro);
+        System.out.println("Char: " + caractere);
     }
 }
