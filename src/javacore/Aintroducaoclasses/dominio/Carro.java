44558ef48905c560e2dac99cdbbf01df6b606a07
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 16:09:32 -0300

    att commit

---
+++ src/javacore/Aintroducaoclasses/dominio/Carro.java
@@ -0,0 +1,7 @@
+package javacore.Aintroducaoclasses.dominio;
+
+public class Carro {
+    public String nome;
+    public String modelo;
+    public int ano;
+}
---
+++ src/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -0,0 +1,26 @@
+package javacore.Aintroducaoclasses.test;
+
+import javacore.Aintroducaoclasses.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        Carro carro2 = new Carro();
+
+        carro1.nome = "Lamborghini";
+        carro1.modelo = "Veneno";
+        carro1.ano = 2020;
+
+        carro2.nome = "Porsche";
+        carro2.modelo = "Carrera GT";
+        carro2.ano = 2022;
+
+        System.out.println("Carro 1");
+        System.out.println("Nome: " + carro1.nome + " | Modelo: " + carro1.modelo + " | Ano: " + carro1.ano);
+
+        System.out.println("----------------------");
+
+        System.out.println("Carro 2");
+        System.out.println("Nome: " + carro2.nome + " | Modelo: " + carro2.modelo + " | Ano: " + carro2.ano);
+    }
+}
