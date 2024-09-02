b4b37d32796d6884f5f86e42a45dd40464d43910
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 15:34:02 -0300

    att commit

---
+++ src/javacore/introducaoclasses/dominio/Pessoa.java
@@ -0,0 +1,7 @@
+package javacore.introducaoclasses.dominio;
+
+public class Pessoa {
+    String nome;
+    int idade;
+    char sexo;
+}
---
+++ src/javacore/introducaoclasses/test/PessoaTest01.java
@@ -0,0 +1,7 @@
+package javacore.introducaoclasses.test;
+
+public class PessoaTest01 {
+    public static void main(String[] args) {
+        
+    }
+}
