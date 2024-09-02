8af412060bf69046186b3e22a84eaab6cbfda7b3
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 11:43:00 -0300

    Initial commit

---
+++ src/academy/devdojo/javamarathon/introduction/Aula01OlaDevDojo.java
@@ -0,0 +1,14 @@
+package academy.devdojo.javamarathon.introduction;
+
+public class Aula01OlaDevDojo {
+    // comentário de uma linha
+
+    /*
+       comentário de
+       múltiplas linhas
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/academy/devdojo/javamarathon/introduction/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javamarathon.introduction;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+
+        int idade = 10;
+        System.out.println("A idade é " + idade + " anos.");
+    }
+}
