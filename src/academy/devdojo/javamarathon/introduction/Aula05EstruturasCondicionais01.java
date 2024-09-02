c7a6b1d6bdf2a01d6de8436cf9f26728219886ba
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-02 13:33:40 -0300

    atualização de aulas

---
+++ src/academy/devdojo/javamarathon/introduction/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.javamarathon.introduction;
+
+public class Aula05EstruturasCondicionais01 {
+    public static void main(String[] args) {
+        int idade = 18;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcoólica.");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcoólica.");
+        }
+    }
+}
---
+++ src/academy/devdojo/javamarathon/introduction/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.javamarathon.introduction;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 18 categoria juvenil
+        // idade > 18 categoria adulto
+
+        int idade = 30;
+        String categoria;
+
+        if (idade < 15) {
+            categoria = "Categoria infantil";
+        } else if (idade >= 15 && idade < 18) {
+            categoria = "Categoria juvenil";
+        } else {
+            categoria = "Categoria adulta";
+        }
+        System.out.println(categoria);
+    }
+}
