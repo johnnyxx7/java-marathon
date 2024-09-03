45cac00fd5bb6318509d640240fe22df4f4d1b30
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-03 11:26:08 -0300

    att commit

--- src/javacore/introducaometodosB/dominio/Calculadora.java
+++ src/javacore/introducaometodosB/dominio/Calculadora.java
@@ -35,4 +35,12 @@ public class Calculadora {
         }
         System.out.println(num1 / num2);
     }
+
+    public void alteraDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumeros");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
 }
---
+++ src/javacore/introducaometodosB/test/CalculadoraTest04.java
@@ -0,0 +1,17 @@
+package javacore.introducaometodosB.test;
+
+import javacore.introducaometodosB.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        int num1 = 1;
+        int num2 = 2;
+
+        calculadora.alteraDoisNumeros(num1, num2);
+        System.out.println("Dentro do CalculadoraTest04");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
