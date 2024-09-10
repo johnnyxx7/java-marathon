d7cf863bc3cc601b0df2ae2187bd6f785e005653
johnnyxx7 <devjohn.araujo@gmail.com>
2024-09-10 16:37:05 -0300

    att commit

--- src/javacore/Dconstrutores/dominio/Anime.java
+++ src/javacore/Dconstrutores/dominio/Anime.java
@@ -5,13 +5,20 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
 
     public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+    }
+
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
     public Anime() {
@@ -24,6 +31,7 @@ public class Anime {
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Episódios: " + this.episodios);
         System.out.println("Gênero: " + this.genero);
+        System.out.println("Estúdio: " + this.estudio);
     }
 
     public void setNome(String nome) {
--- src/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/javacore/Dconstrutores/test/AnimeTest01.java
@@ -6,8 +6,5 @@ public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime("No Game No Life", "Ação", 12, "Isekai");
         anime.imprime();
-
-        Anime anime2 = new Anime();
-        anime2.imprime();
     }
 }
