599c8da8b0f2bcf9904b16cd6cc5a3f3f106357d
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-02 10:19:36 -0300

    att commit

---
+++ src/javacore/Fmodificadoresestaticos/dominio/Carro.java
@@ -0,0 +1,44 @@
+package javacore.Fmodificadoresestaticos.dominio;
+
+public class Carro {
+
+    private String nome;
+    private double velocidadeMaxima;
+    public static double velocidadeLimite = 250;
+
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public void imprime() {
+        System.out.println("--------------------");
+        System.out.println("nome: " + this.nome);
+        System.out.println("velocidade máxima: " + this.velocidadeMaxima);
+        System.out.println("velocidade limite: " + Carro.velocidadeLimite);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public void setVelocidadeLimite(double velocidadeLimite) {
+        this.velocidadeLimite = velocidadeLimite;
+    }
+}
---
+++ src/javacore/Fmodificadoresestaticos/test/CarroTest01.java
@@ -0,0 +1,17 @@
+package javacore.Fmodificadoresestaticos.test;
+
+import javacore.Fmodificadoresestaticos.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+
+        Carro.velocidadeLimite = 180;
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}
