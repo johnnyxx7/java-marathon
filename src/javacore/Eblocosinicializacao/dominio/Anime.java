7c95f146b3141aaf8f7d1c336cbebe2f8695c294
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-02 09:44:33 -0300

    att commit

--- src/javacore/Eblocosinicializacao/dominio/Anime.java
+++ src/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -2,11 +2,39 @@ package javacore.Eblocosinicializacao.dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodios = {1,2,3,4,5,6,7,8,9,10,11,12};
+    private int[] episodios;
+
+    // 1 - alocado espaço memória para o objeto
+    // 2 - cada atributo de classe é criado e inicializado com valores default ou o que for passado
+    // 3 - bloco de inicialização é executado
+    // 4 - construtor é executado
+
+    {
+        System.out.println("dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
 
     public Anime() {
-        for (int episodios : this.episodios) {
-            System.out.println(episodios + " ");
+        for(int episodio : this.episodios) {
+            System.out.print(episodio + " ");
         }
+
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
     }
 }
--- src/javacore/Eblocosinicializacao/test/AnimeTest01.java
+++ src/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -5,5 +5,9 @@ import javacore.Eblocosinicializacao.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime();
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.print(episodio + " ");
+        }
     }
 }
