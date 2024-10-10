94e20b09dd1734053086aa198095e781957d4d82
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-10 14:21:53 -0300

    att commit

--- src/javacore/Gassociacao/dominio/Time.java
+++ src/javacore/Gassociacao/dominio/Time.java
@@ -2,9 +2,23 @@ package javacore.Gassociacao.dominio;
 
 public class Time {
     private String nome;
+    private Jogador[] jogadores;
 
     public Time(String nome) {
         this.nome = nome;
+    }
+
+    public Time(String nome, Jogador[] jogadores) {
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
+
+    public void imprime() {
+        System.out.println("Nome: " + this.nome);
+        if (jogadores == null) return;
+        for (Jogador jogador : jogadores) {
+            System.out.println("Jogador: " + jogador.getNome());
+        }
     }
 
     public String getNome() {
