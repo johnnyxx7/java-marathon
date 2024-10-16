19bba22dc22764eb4fc009fb84552d322def9c0e
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-16 15:39:33 -0300

    att commit

--- src/javacore/Kenum/dominio/Cliente.java
+++ src/javacore/Kenum/dominio/Cliente.java
@@ -1,11 +1,6 @@
 package javacore.Kenum.dominio;
 
 public class Cliente {
-    public enum TipoPagamento {
-        DEBITO,
-        CREDITO
-    }
-
     private String nome;
     private TipoCliente tipoCliente;
     private TipoPagamento tipoPagamento;
--- src/javacore/Kenum/test/ClienteTest01.java
+++ src/javacore/Kenum/test/ClienteTest01.java
@@ -2,13 +2,17 @@ package javacore.Kenum.test;
 
 import javacore.Kenum.dominio.Cliente;
 import javacore.Kenum.dominio.TipoCliente;
+import javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Johnny", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
-        Cliente cliente2 = new Cliente("Johnny", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente1 = new Cliente("Johnny", TipoCliente.PESSOA_FISICA, TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("Johnny", TipoCliente.PESSOA_JURIDICA, TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
+
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
