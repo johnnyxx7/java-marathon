11bc5f398f069bb851b9323fed5176a18703de09
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-16 15:03:02 -0300

    att commit

--- src/javacore/Kenum/dominio/TipoCliente.java
+++ src/javacore/Kenum/dominio/TipoCliente.java
@@ -1,6 +1,22 @@
 package javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
+
+    private int valor;
+    private String nomeRelatorio;
+
+   TipoCliente(int valor, String nomeRelatorio) {
+        this.valor = valor;
+        this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public String getNomeRelatorio() {
+        return nomeRelatorio;
+    }
 }
--- src/javacore/Kenum/test/ClienteTest01.java
+++ src/javacore/Kenum/test/ClienteTest01.java
@@ -5,8 +5,8 @@ import javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Johnny", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Johnny", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Johnny", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente2 = new Cliente("Johnny", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.DEBITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
