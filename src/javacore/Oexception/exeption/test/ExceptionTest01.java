4862a1dbecd3411e80ed4a05bdc011416fb2893c
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-23 14:49:12 -0300

    att commit

---
+++ src/javacore/Oexception/exeption/test/ExceptionTest01.java
@@ -0,0 +1,20 @@
+package javacore.Oexception.exeption.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarArquivo();
+    }
+
+    private static void criarArquivo() {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -2,7 +2,7 @@ package javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest01 {
     public static void main(String[] args) {
-        // CHECKED E UNCHECKED
+        // Checked e Unchecked
 
         int[] nums = {1, 2};
         System.out.println(nums[2]);
