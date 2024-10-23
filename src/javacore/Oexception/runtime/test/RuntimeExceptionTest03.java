f5875ccafa94873da644219952f0e8271b730ddc
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-23 17:12:34 -0300

    att commit

--- src/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
+++ src/javacore/Oexception/runtime/test/RuntimeExceptionTest03.java
@@ -2,6 +2,28 @@ package javacore.Oexception.runtime.test;
 
 public class RuntimeExceptionTest03 {
     public static void main(String[] args) {
-        
+        abreConexao();
+    }
+
+    public static String abreConexao() {
+        try {
+            System.out.println("Abrindo arquivo.");
+            System.out.println("Escrevendo dados no arquivo.");
+            return "conexão aberta";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Fechando recursos liberados pelo SO.");
+        }
+        return null;
+    }
+
+    public static void abreConexao2() {
+        try {
+            System.out.println("Abrindo arquivo.");
+            System.out.println("Escrevendo dados no arquivo.");
+        } finally {
+            System.out.println("Fechando recursos liberados pelo SO.");
+        }
     }
 }
