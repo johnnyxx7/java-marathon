c368734f9a8fa5f88daab4a23c0c6cc4e5092903
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-29 17:37:19 -0300

    att commit

---
+++ src/javacore/Oexception/exception/dominio/Leitor2.java
@@ -0,0 +1,12 @@
+package javacore.Oexception.exception.dominio;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class Leitor2 implements Closeable {
+
+    @Override
+    public void close() throws IOException {
+        System.out.println("Fechando o leitor 2");
+    }
+}
--- src/javacore/Oexception/exception/test/TryWithResourcesTest01.java
+++ src/javacore/Oexception/exception/test/TryWithResourcesTest01.java
@@ -1,7 +1,38 @@
 package javacore.Oexception.exception.test;
 
+import javacore.Oexception.exception.dominio.Leitor1;
+import javacore.Oexception.exception.dominio.Leitor2;
+
+import java.io.*;
+
 public class TryWithResourcesTest01 {
     public static void main(String[] args) {
-        
+        lerArquivo();
+    }
+
+    public static void lerArquivo() {
+        try (Leitor1 leitor1 = new Leitor1();
+             Leitor2 leitor2 = new Leitor2()) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("texto.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
     }
 }
