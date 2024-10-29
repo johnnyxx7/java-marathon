2c804f8b0eabb50ba6e9fba843f8713fe0552ce7
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-29 16:59:13 -0300

    att commit

---
+++ src/javacore/Oexception/exception/test/TryWithResourcesTest01.java
@@ -0,0 +1,7 @@
+package javacore.Oexception.exception.test;
+
+public class TryWithResourcesTest01 {
+    public static void main(String[] args) {
+        
+    }
+}
--- src/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ src/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -7,23 +7,15 @@ public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try {
             throw new ArrayIndexOutOfBoundsException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
-        } catch (ArithmeticException e) {
-            System.out.println("Dentro do ArithmeticException");
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
         } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (SQLException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
