7dbca82770d99f69804125489aafa8e0375a0e72
johnnyxx7 <devjohn.araujo@gmail.com>
2024-10-29 16:04:00 -0300

    att commit

---
+++ src/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -0,0 +1,32 @@
+package javacore.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro do IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+
+        try {
+            talvezLanceException();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException {}
+}
