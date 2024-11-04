0584a22d8e7bad6066102eaf31963bfba195c94d
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-04 15:39:23 -0300

    att commit

--- src/javacore/Qstring/test/StringTest02.java
+++ src/javacore/Qstring/test/StringTest02.java
@@ -3,10 +3,14 @@ package javacore.Qstring.test;
 public class StringTest02 {
     public static void main(String[] args) {
         String nome = "Maysa";
+        String numeros = "012345";
         System.out.println(nome.charAt(0));
         System.out.println(nome.length());
         System.out.println(nome.replace("s", "l"));
         System.out.println(nome.toLowerCase());
         System.out.println(nome.toUpperCase());
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(0, numeros.length()));
+        System.out.println(nome.trim());
     }
 }
