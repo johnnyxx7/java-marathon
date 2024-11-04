d86eda9adbc1bbe0801e68325d949e8d6270ec4f
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-04 15:33:38 -0300

    att commit

--- src/javacore/Qstring/test/StringTest01.java
+++ src/javacore/Qstring/test/StringTest01.java
@@ -4,7 +4,7 @@ public class StringTest01 {
    public static void main(String[] args) {
        String nome = "Johnny"; // String constant pool
        String nome2 = "Johnny";
-       nome = nome.concat(" Araujo");
+       nome = nome.concat(" Araujo"); // nome += " Araujo"
        System.out.println(nome);
        System.out.println(nome == nome2);
        String nome3 = new String("Johnny"); // 1. uma variavel de referencia 2. objeto do tipo string 3. uma string no pool
---
+++ src/javacore/Qstring/test/StringTest02.java
@@ -0,0 +1,12 @@
+package javacore.Qstring.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "Maysa";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("s", "l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+    }
+}
