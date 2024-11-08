2a175e27904c53c0336a6ecfb7c7fd2426f67f6c
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-08 10:03:34 -0300

    att commit

--- src/javacore/Rdatas/test/DateTest01.java
+++ src/javacore/Rdatas/test/DateTest01.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class DateTest01 {
     public static void main(String[] args) {
-        Date date = new Date(1731006246866L);
+        Date date = new Date();
+        date.setTime(date.getTime() + 3_600_000);
         System.out.println(date);
-
     }
 }
