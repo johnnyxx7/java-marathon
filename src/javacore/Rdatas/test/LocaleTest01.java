cf096405444ac22967e682c02db9b845b43261a2
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-11 15:01:57 -0300

    att commit

---
+++ src/javacore/Rdatas/test/LocaleTest01.java
@@ -0,0 +1,17 @@
+package javacore.Rdatas.test;
+
+import java.util.Scanner;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("The number " + number + " is even.");
+        } else {
+            System.out.println("The number " + number + " is odd.");
+        }
+    }
+}
