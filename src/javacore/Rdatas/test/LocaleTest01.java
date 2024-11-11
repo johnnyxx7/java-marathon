2f2b131ae0960ec8413be5246e358bb059de02ec
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-11 15:43:10 -0300

    att commit

--- src/javacore/Rdatas/test/LocaleTest01.java
+++ src/javacore/Rdatas/test/LocaleTest01.java
@@ -1,17 +1,37 @@
 package javacore.Rdatas.test;
 
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class LocaleTest01 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        // pt-br
 
-        if (number % 2 == 0) {
-            System.out.println("The number " + number + " is even.");
-        } else {
-            System.out.println("The number " + number + " is odd.");
-        }
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeItalyCH = new Locale("it", "CH");
+        Locale localeIndia = new Locale("hi", "IN");
+        Locale localeJapao = new Locale("ja", "JP");
+        Locale localeHolanda = new Locale("nl", "NL");
+        Locale localeBrasil = new Locale("pt", "BR");
+
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL, localeItalyCH);
+        DateFormat df3 = DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+        DateFormat df4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapao);
+        DateFormat df5 = DateFormat.getDateInstance(DateFormat.FULL, localeHolanda);
+        DateFormat df6 = DateFormat.getDateInstance(DateFormat.FULL, localeBrasil);
+
+        System.out.println("Itália: " + df1.format(calendar.getTime()));
+        System.out.println("Suíça: " + df2.format(calendar.getTime()));
+        System.out.println("India: " + df3.format(calendar.getTime()));
+        System.out.println("Japão: " + df4.format(calendar.getTime()));
+        System.out.println("Holanda: " + df5.format(calendar.getTime()));
+        System.out.println("Brasil: " + df6.format(calendar.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry());
+        System.out.println(localeItalyCH.getDisplayCountry());
     }
 }
