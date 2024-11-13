8b344356d7df202c8c07f425c5c8ebfc40023cf9
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-13 18:20:32 -0300

    att commit

--- src/javacore/Rdatas/test/LocalTimeTest01.java
+++ src/javacore/Rdatas/test/LocalTimeTest01.java
@@ -6,7 +6,11 @@ public class LocalTimeTest01 {
     public static void main(String[] args) {
         LocalTime time = LocalTime.of(21, 7, 12);
         LocalTime timeNow = LocalTime.now();
-        System.out.println(time);
-        System.out.println(timeNow);
+        System.out.println("Hora criada: " + time);
+        System.out.println("Hora atual: " + timeNow);
+        System.out.println("Hora: " + timeNow.getHour());
+        System.out.println("Minuto: " + timeNow.getMinute());
+        System.out.println("Segundo: " + timeNow.getSecond());
+        System.out.println("Nanosegundo: " + timeNow.getNano());
     }
 }
