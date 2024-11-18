598ca55537e552ee453ba6051123987f1c3e3271
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-18 17:37:52 -0300

    att commit

---
+++ src/javacore/Rdatas/test/DurationTest01.java
@@ -0,0 +1,21 @@
+package javacore.Rdatas.test;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DurationTest01 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2).plusMinutes(8);
+        LocalTime timeNow = LocalTime.now();
+        LocalTime timeMinus7Hours = LocalTime.now().minusHours(7);
+        Duration d1 = Duration.between(now, nowAfterTwoYears);
+        Duration d2 = Duration.between(timeNow, timeMinus7Hours);
+        Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+    }
+}
