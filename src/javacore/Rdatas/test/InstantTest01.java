da759e68f0d37aa7f91e94399816c2672e562683
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-18 16:42:06 -0300

    att commit

---
+++ src/javacore/Rdatas/test/InstantTest01.java
@@ -0,0 +1,12 @@
+package javacore.Rdatas.test;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+public class InstantTest01 {
+    public static void main(String[] args) {
+        Instant now = Instant.now();
+        System.out.println(now);
+        System.out.println(LocalDateTime.now());
+    }
+}
