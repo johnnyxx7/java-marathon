ad3d1c5ccca556cfcc670ed1e0d896b889d7538a
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-19 10:17:44 -0300

    att commit

---
+++ src/javacore/Rdatas/test/PeriodTest01.java
@@ -0,0 +1,19 @@
+package javacore.Rdatas.test;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class PeriodTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2).plusDays(7);
+        Period p1 = Period.between(now, nowAfterTwoYears);
+        Period p2 = Period.ofDays(10);
+        Period p3 = Period.ofWeeks(58);
+        Period p4 = Period.ofMonths(5);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+    }
+}
