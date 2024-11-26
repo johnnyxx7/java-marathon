0e48760fb7b8a4ea388cf82141a8162433c3843d
johnnyxx7 <devjohn.araujo@gmail.com>
2024-11-26 16:54:05 -0300

    att commit

---
+++ src/javacore/Sformatacao/test/DateFormatterTest01.java
@@ -0,0 +1,16 @@
+package javacore.Sformatacao.test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class DateFormatterTest01 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        String s1 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+        String s2 = date.format(DateTimeFormatter.ISO_DATE);
+        String s3 = date.format(DateTimeFormatter.ISO_LOCAL_DATE);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
